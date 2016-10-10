d2503f51d175a074b31a304d96a629101b6e0919
Brandon Knitter <knitterb@blandsite.org>
2016-10-10 15:47:17 -0700

    Fixed hash creation and text length restriction modified hash creation logic (make it a string, duh) added debugging for task being attempted added logic to reduce maximum text length removed redundant semicolons (Kotlin)

--- src/main/java/nl/mpcjanssen/simpletask/PebbleReceiver.kt
+++ src/main/java/nl/mpcjanssen/simpletask/PebbleReceiver.kt
@@ -50,6 +50,7 @@ class PebbleReceiver(subscribedUuid: UUID?) : PebbleDataReceiver(subscribedUuid)
 
         @JvmStatic var nextTask=0;
         @JvmStatic fun sendNextTask(context: Context?) {
+            log.debug(TAG, "Sending task #"+nextTask)
             if (TodoList.todoItems.size <= nextTask) {
                 log.debug(TAG, "No more tasks to send at "+ nextTask);
                 return;
@@ -62,34 +63,51 @@ class PebbleReceiver(subscribedUuid: UUID?) : PebbleDataReceiver(subscribedUuid)
             // just get the text portion out for now
             val tokensToShow = TToken.ALL and TToken.TEXT
             // TODO(bk) need to trim this to a maximum length
-            val text = item.task.showParts(tokensToShow)
+            val text_s = item.task.showParts(tokensToShow)
+            val text:String
+            if (text_s.length<=25)
+                text=text_s
+            else
+                text=text_s.substring(0,25)
 
             // generate hash
             val md=MessageDigest.getInstance("MD5")
-            md.update(item.task.text.toByte())
-            val hash=String(md.digest()).substring(21,31)
+            md.update(item.task.text.toByteArray())
+            val hash_b = md.digest()
+            var hash_s:StringBuffer= StringBuffer()
+            for (i in hash_b.indices) {
+                if (0xff and hash_b[i].toInt() < 0x10) {
+                    hash_s.append("0" + Integer.toHexString(0xFF and hash_b[i].toInt()))
+                } else {
+                    hash_s.append(Integer.toHexString(0xFF and hash_b[i].toInt()))
+                }
+            }
+            val hash=hash_s.substring(hash_s.length-10,hash_s.length)
 
-            dict.addInt32(0, MessageTypeResponseTask);
-            dict.addInt32(MessageTypeResponseTaskId, item.line.toInt());
+
+
+            dict.addInt32(0, MessageTypeResponseTask)
+            dict.addInt32(MessageTypeResponseTaskId, item.line.toInt())
             dict.addString(MessageTypeResponseTaskHash, hash)
             dict.addString(MessageTypeResponseTaskName, text)
 
-            log.debug(TAG, "Sending task: " + item.line + "=" + text+" with UUID: "+ appUuid);
+            log.debug(TAG, "Sending task: " + item.line + "=" + text+" with UUID: "+ appUuid)
+            log.debug(TAG, "Dict size: "+dict.size())
             PebbleKit.sendDataToPebbleWithTransactionId(context, appUuid, dict, 42)
         }
 
 
         // Watch to Phone request types (field 0)
-        val MessageTypeRequestAllTasks:Long=0;
-        val MessageTypeRequestCompleteTask=1;
+        val MessageTypeRequestAllTasks:Long=0
+        val MessageTypeRequestCompleteTask=1
 
         // Phone to Watch Response Types (field 0)
-        val MessageTypeResponseTask=0;
+        val MessageTypeResponseTask=0
 
         // Fields: MessageTypeResponseTask=0
-        val MessageTypeResponseTaskId=1;
-        val MessageTypeResponseTaskHash=2;
-        val MessageTypeResponseTaskName=3;
+        val MessageTypeResponseTaskId=1
+        val MessageTypeResponseTaskHash=2
+        val MessageTypeResponseTaskName=3
 
         private val TAG = "PebbleReceiver"
     }
